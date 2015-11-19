4fd6821ad0634bf332a67a4472b15fb2d93ec5af
jeffthemaximum <frey.maxim@gmail.com>
2015-11-19 15:45:41 -0500

    postgres

--- jeffsite/settings.py
+++ jeffsite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'jeffsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jeffsite',
+        'USER': 'curtisfinnigan',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'POST': '',
     }
 }
 
